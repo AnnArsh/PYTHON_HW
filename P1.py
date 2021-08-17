ee14941b6324c92e39e4fdc93fec2ad9dcb58bb7
AnnArsh <89065260+AnnArsh@users.noreply.github.com>
2021-08-17 10:39:37 +0400

    Add files via upload

---
+++ P1.py
@@ -0,0 +1,5 @@
+list_1 = [5, 10, 15, 20, 25, 30, 80, 20, 5]
+ind20 = list_1.index(20)
+list_1.remove(20)
+list_1.insert(ind20, 200)
+print(list_1)
---
+++ P5.py
@@ -0,0 +1,6 @@
+# dic = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+# dic.pop('a')
+# print(dic)
+dic = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+del dic['a']
+print(dic)
